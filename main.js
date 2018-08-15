69b6d7639f947fbcc6e8bbd6fc4d5bcfeb3a85c0
eyalshnitzer <41183382+eyalshnitzer@users.noreply.github.com>
2018-08-15 13:39:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,138 @@
+// search logic
+// items logic 
+// item click logic
+
+
+// random array functions
+var initiateArray = ((size) => {
+	var arr = [];
+	for (var i=0;i<size;i++) {
+		arr[i] = Math.round(Math.random() * 1);
+	}
+	return arr;
+})
+
+// data
+var movies = [
+	{
+		name: "The Shawshank Redemption",
+		length: 144,
+		id: "a1d944f9-7f0f-4eb3-a978-bd62329a9e9b",
+		rank: 9.3,
+		image: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+		vip_chairs: initiateArray(10),
+		regular_chairs: initiateArray(30),
+		comments: []
+	},
+	{
+		name: "The Godfather",
+		length: 175,
+		id: "b649cd6c-c5a5-40cf-be81-44de946cbc59",
+		rank: 9.2,
+		image: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,704,1000_AL_.jpg",
+		vip_chairs: initiateArray(10),
+		regular_chairs: initiateArray(30),
+		comments: []
+	},
+	{
+		name: "Godfather: Part II",
+		length: 202,
+		id: "b649cd6c-c5a5-40cf-be81-44de946cbc60",
+		rank: 9.0,
+		image: "https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,701,1000_AL_.jpg",
+		vip_chairs: initiateArray(10),
+		regular_chairs: initiateArray(30),
+		comments: []
+	},
+	{
+	name: "The Dark Knight",
+		length: 152,
+		id: "b649cd6c-c5a5-40cf-be81-44de946cbc60",
+		rank: 9.0,
+		image: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		vip_chairs: initiateArray(10),
+		regular_chairs: initiateArray(30),
+		comments: []
+	},
+	{
+		name: "Inception",
+		length: 148,
+		id: "b649cd6c-c5a5-40cf-be81-44de946cbc61",
+		rank: 8.8,
+		image: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		vip_chairs: initiateArray(10),
+		regular_chairs: initiateArray(30),
+		comments: []
+	},
+	{
+		name: "Pulp Fiction",
+		length: 154,
+		id: "b649cd6c-c5a5-40cf-be81-44de946cbc62",
+		rank: 8.9,
+		image: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR1,0,182,268_AL_.jpg",
+		vip_chairs: initiateArray(10),
+		regular_chairs: initiateArray(30),
+		comments: []	
+	},
+	{
+		name: "Fight Club",
+		length: 139,
+		id: "b649cd6c-c5a5-40cf-be81-44de946cbc63",
+		rank: 8.8,
+		image: "https://m.media-amazon.com/images/M/MV5BNGM2NjQxZTAtMmU5My00YTk5LWFmOWMtYjZlYzk4YzMwNjFlXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		vip_chairs: initiateArray(10),
+		regular_chairs: initiateArray(30),
+		comments: []
+	},
+	{	
+		name: "Forrest Gump",
+		length: 142,
+		id: "b649cd6c-c5a5-40cf-be81-44de946cbc64",
+		rank: 8.8,
+		image: "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR1,0,182,268_AL_.jpg",
+		vip_chairs: initiateArray(10),
+		regular_chairs: initiateArray(30),
+		comments: []	
+	},
+	{
+		name: "The usual suspects",
+		length: 106,
+		id: "b649cd6c-c5a5-40cf-be81-44de946cbc65",
+		rank: 8.6,
+		image: "https://m.media-amazon.com/images/M/MV5BYTViNjMyNmUtNDFkNC00ZDRlLThmMDUtZDU2YWE4NGI2ZjVmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		vip_chairs: initiateArray(10),
+		regular_chairs: initiateArray(30),
+		comments: []
+	},
+	{
+		name: "27 Dresses",
+		length: 111,
+		id: "b649cd6c-c5a5-40cf-be81-44de946cbc65",
+		rank: 6.1,
+		image: "https://m.media-amazon.com/images/M/MV5BMzI5OTM0OTg2MF5BMl5BanBnXkFtZTcwNjAyMTU1MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+		vip_chairs: initiateArray(10),
+		regular_chairs: initiateArray(30),
+		comments: []
+	}
+]
+
+var contentDiv = document.getElementById("content");
+
+movies.forEach(item => {
+    contentDiv.innerHTML += `<div class="movieItem">
+                                <div class="movieImg"> <img src="${item.image}"/></div>
+                                <div class="movieTitle">${item.name}</div>
+                                
+                                
+                                <div class="buttomMovieItem">
+                                    <div class="movieLength">${item.length}</div>
+                                    <div class="movieRank">${item.rank}</div>
+                                </div>
+                            </div>`
+})
+
+var searchText = document.getElementById("textSearchMovies");
+searchText.addEventListener("keyup", function(evt){
+	var newArray = movies.filter((item) =>  item.name.indexOf(this.value) > -1)
+    console.log(newArray);
+});
