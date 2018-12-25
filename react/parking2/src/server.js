da7f9f4ee6cdc4376410deed4f27a047709e4a1c
maxim <admtrigger@gmail.com>
2018-12-25 18:04:19 +0200

    all
    all

--- react/parking2/src/server.js
+++ react/parking2/src/server.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 app.use(express.static('dist'));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.listen(port);
-console.log(`Server running on port ${port}`);
+const users = [
+   { user:"test@test.com", password: "1234", fullName: "Testy Test"},
+   { user:"test2@test.com", password: "4321", fullName: "Tester Test"}
+]
+
+const cars = [
+   { id: "1234567", owner: "test@test.com"},
+   { id: "7654321", owner: "test@test.com"}
+]
+
+app.post("/login", (req,res) => {
+   let result={};
+   if (req.body) {
+      console.log("req.body", req.body);
+      let item = users.find(u => u.user == req.body.username && u.password == req.body.password);
+      if (item) {
+         res.json({name: item.fullName})
+      }
+   }
+   res.json(result);
+
+});
+
+app.get("/user", (req,res) => {
+
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
